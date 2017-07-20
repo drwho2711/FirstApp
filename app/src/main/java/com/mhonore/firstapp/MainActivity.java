9cc806d05ea03e53330d1c1b812a6cd869d6de53
drwho2711 <marissaleon225@gmail.com>
2017-07-20 16:30:57 -0500

    first commit

---
+++ app/src/main/java/com/mhonore/firstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mhonore.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void getHelloWorldText (View view){
+        Log.i("info", "Button Pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
